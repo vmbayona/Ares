66d10b3c71624c70efe8345a580f370415d1c779
vmbayona <107092558+vmbayona@users.noreply.github.com>
2023-10-24 21:53:44 -0500

    Add files via upload
    Se sube SQL de correo de vendedores

---
+++ BI/Querys_AW/Correo_Vendedores.sql
@@ -0,0 +1,17 @@
+SELECT 
+    SP.BusinessEntityID AS VendedorID,
+    P.FirstName AS Nombre,
+    P.LastName AS Apellido,
+    CONCAT(
+        LOWER(LEFT(P.FirstName, 1)), 
+        LOWER(P.LastName), 
+        RIGHT(CAST(SP.BusinessEntityID AS VARCHAR), 2),
+        '@adventureworks.com'
+    ) AS CorreoElectronico
+FROM 
+    Sales.SalesPerson AS SP
+JOIN 
+    Person.Person AS P
+ON 
+    SP.BusinessEntityID = P.BusinessEntityID;
+
